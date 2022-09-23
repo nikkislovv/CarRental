f7eed7ea7f1ee3f70cb0cbef0275e7f8bcba9914
Nikita <99506812+nikkislovv@users.noreply.github.com>
2022-09-23 14:01:07 +0300

    Addiing ServiceExtensions and Configuring settings

---
+++ Server/Extensions/ServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Server.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+            });
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,19 +1,26 @@
+using Server.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
 
 // Add services to the container.
+services.ConfigureIISIntegration();
 
-builder.Services.AddControllers();
+services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
