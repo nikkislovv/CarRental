bea763ffe706f177d72cf91eaaa194dae6e928d7
Nikita <99506812+nikkislovv@users.noreply.github.com>
2022-09-23 14:17:07 +0300

    Adding Models

---
+++ Entities/Models/Rent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Rent
+    {
+        public Guid Id { get; set; }
+        public string ClientName { get; set; }
+        public Guid CarId { get; set; }
+        public virtual Car Car { get; set; }
+    }
+}
